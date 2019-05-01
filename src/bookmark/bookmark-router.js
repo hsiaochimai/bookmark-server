ca252db9f21d0990e31811ebb84405e8901ba5e2
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-05-01 16:48:36 +0300

    wrote some tests

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -4,69 +4,69 @@ const logger = require("../logger");
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 const bookmarks = require("../store");
-const BASE_URL=process.env.BASE_URL
+const BASE_URL = process.env.BASE_URL
 const { isWebUri } = require('valid-url')
 bookmarkRouter
-.route("/bookmarks")
-.get((req, res) => {
-  res.json(bookmarks);
-})
-.post(bodyParser,(req,res)=>{
-    const {title, url, desc, rating}=req.body;
+  .route("/bookmarks")
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, desc, rating } = req.body;
     console.log(req.body)
-    if(!title){
-        logger.error(`Title is required`);
+    if (!title) {
+      logger.error(`Title is required`);
       return res.status(400).send("Invalid data");
     }
-    if(!url|| !isWebUri(url)){
-        logger.error(`url is invalid or missing`);
-        return res.status(400).send("Invalid data"); 
-    }
-   if(!desc){
-    logger.error(`Description is required`);
+    if (!url || !isWebUri(url)) {
+      logger.error(`url is invalid or missing`);
       return res.status(400).send("Invalid data");
-   }
-   if (!Number.isInteger(rating) || rating < 0 || rating > 5){
-    logger.error(`'${rating}'is invalid`);
-    return res.status(400).send("Invalid data");
-   }
-   const id= uuid();
-   const bookmark= {
-       id,
-       title,
-       url,
-       desc,
-       rating
-   };
-   bookmarks.push(bookmark)
-   logger.info(`bookmark with id ${id} created`);
+    }
+    if (!desc) {
+      logger.error(`Description is required`);
+      return res.status(400).send("Invalid data");
+    }
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`'${rating}'is invalid`);
+      return res.status(400).send("Invalid data");
+    }
+    const id = uuid();
+    const bookmark = {
+      id,
+      title,
+      url,
+      desc,
+      rating
+    };
+    bookmarks.push(bookmark)
+    logger.info(`bookmark with id ${id} created`);
 
     res
       .status(201)
       .location(`${BASE_URL}bookmarks/${id}`)
       .json(bookmark);
-})
+  })
 bookmarkRouter
-.route("/bookmarks/:id")
-.get((req, res) => {
-  const { id } = req.params;
-  const bookmark = bookmarks.find(bm => bm.id == id);
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(bm => bm.id == id);
 
-  if (!bookmark) {
-    logger.error(`bookmark with id ${id} not found.`);
-    return res.status(404).send("Bookmark Not Found");
-  }
-  res.json(bookmark);
-})
-.delete((req,res)=>{
-    const {id}=req.params;
-    const bmIndex=bookmarks.findIndex(item=>item.id===id)
-    if(bmIndex===-1){
-        logger.error(`bookmark with id ${id} not found.`);
-        return res.status(404).send('You can not delete a card that does not exist')
+    if (!bookmark) {
+      logger.error(`bookmark with id ${id} not found.`);
+      return res.status(404).send("Bookmark Not Found");
     }
-bookmarks.splice(bmIndex, 1)
-logger.info(`bookmark with id ${id} deleted`);
-return res.status(204).send()
-})
+    res.status(200).json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bmIndex = bookmarks.findIndex(item => item.id === id)
+    if (bmIndex === -1) {
+      logger.error(`bookmark with id ${id} not found.`);
+      return res.status(404).send('You can not delete a card that does not exist')
+    }
+    bookmarks.splice(bmIndex, 1)
+    logger.info(`bookmark with id ${id} deleted`);
+    return res.status(204).send()
+  })
 module.exports = bookmarkRouter;
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,82 @@
-const app = require('../src/app')
+const mocha = require('mocha')
+const expect = require('chai').expect;
+const describe = mocha.describe
+const it = mocha.it
+const bookmarks = require('../src/store')
 
+const app = require('../src/app')
+const supertest = require('supertest')
+const { API_TOKEN } = process.env
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET /bookmarks validates API_TOKEN', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+      .get(`/bookmarks`)
+      .set('Authorization', `Bearer fake${API_TOKEN}`) //set auth header
+      .expect(401)
+      .then(res => {
+        expect(res.body.error).to.equal("Unauthorized request")
+        // expect(res.body.length).to.equal(2)
+        
+      });
   })
+
+  it('GET /bookmarks responds with an array of bookmarks', () => {
+    return supertest(app)
+      .get(`/bookmarks`)
+      .set('Authorization', `Bearer ${API_TOKEN}`) //set auth header
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        expect(res.body.length).to.equal(2)
+      });
+  })
+
+  it('GET /bookmarks/:id responds with a bookmark', () => {
+    const id = '8sdfbvbs65sd'
+    return supertest(app)
+      .get(`/bookmarks/${id}`)
+      .set('Authorization', `Bearer ${API_TOKEN}`) //set auth header
+      .expect(200)
+      .then(res => {
+        const storeBm = bookmarks.find(i => i.id === id)
+        expect(res.body).to.be.an('object')
+        expect(res.body).to.deep.equal(storeBm);
+
+      });
+  })
+
+  it('POST /bookmarks adds a bookmark to the store', () => {
+
+    const newBm = {
+      "title": "New!",
+      "url": "http://medium.com/",
+      "desc": "Reading time",
+      "rating": 3
+    }
+
+    return supertest(app)
+      .post('/bookmarks')
+      .set('Authorization', `Bearer ${API_TOKEN}`) //set auth header
+      .send(newBm)
+      .expect(201)
+      .then(() => {
+        const item = bookmarks.find(i => i.url === newBm.url && i.title === newBm.title)
+        expect(item).to.not.be.undefined;
+        expect(item.id).to.not.be.undefined;
+      });
+  })
+  it('DELETE /bookmarks/id deletes a bookmark', () => {
+
+    const id = '8sdfbvbs65sd'
+    return supertest(app)
+      .delete(`/bookmarks/${id}`)
+      .set('Authorization', `Bearer ${API_TOKEN}`) //set auth header
+      // .send(newBm)
+      .expect(204)
+      .then(() => {
+        const item = bookmarks.find(i => i.id === id)
+        expect(item).to.be.undefined;
+      });
+  })
+
 })
