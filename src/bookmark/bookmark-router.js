c16409e70a13e58a755ece4e7aa468420c71d3a7
Jennifer Hsiao <jchmai21@gmail.com>
2019-05-01 05:53:18 -0700

    error handling

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -5,6 +5,7 @@ const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 const bookmarks = require("../store");
 const BASE_URL=process.env.BASE_URL
+const { isWebUri } = require('valid-url')
 bookmarkRouter
 .route("/bookmarks")
 .get((req, res) => {
@@ -17,16 +18,16 @@ bookmarkRouter
         logger.error(`Title is required`);
       return res.status(400).send("Invalid data");
     }
-    if(!url){
-        logger.error(`url is required`);
+    if(!url|| !isWebUri(url)){
+        logger.error(`url is invalid or missing`);
         return res.status(400).send("Invalid data"); 
     }
    if(!desc){
     logger.error(`Description is required`);
       return res.status(400).send("Invalid data");
    }
-   if (!rating){
-    logger.error(`Rating is required`);
+   if (!Number.isInteger(rating) || rating < 0 || rating > 5){
+    logger.error(`'${rating}'is invalid`);
     return res.status(400).send("Invalid data");
    }
    const id= uuid();
@@ -45,7 +46,9 @@ bookmarkRouter
       .location(`${BASE_URL}bookmarks/${id}`)
       .json(bookmark);
 })
-bookmarkRouter.route("/bookmarks/:id").get((req, res) => {
+bookmarkRouter
+.route("/bookmarks/:id")
+.get((req, res) => {
   const { id } = req.params;
   const bookmark = bookmarks.find(bm => bm.id == id);
 
@@ -54,5 +57,16 @@ bookmarkRouter.route("/bookmarks/:id").get((req, res) => {
     return res.status(404).send("Bookmark Not Found");
   }
   res.json(bookmark);
-});
+})
+.delete((req,res)=>{
+    const {id}=req.params;
+    const bmIndex=bookmarks.findIndex(item=>item.id===id)
+    if(bmIndex===-1){
+        logger.error(`bookmark with id ${id} not found.`);
+        return res.status(404).send('You can not delete a card that does not exist')
+    }
+bookmarks.splice(bmIndex, 1)
+logger.info(`bookmark with id ${id} deleted`);
+return res.status(204).send()
+})
 module.exports = bookmarkRouter;
