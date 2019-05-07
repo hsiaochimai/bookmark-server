c22f7703b51f5326926c20005514082977200f14
Jennifer Hsiao <jchmai21@gmail.com>
2019-05-07 05:34:18 -0700

    migrations/ seed

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+insert into bookmarks (title, url_link, descript, rating)
+values
+  ('Thinkful', 'https://www.thinkful.com/','My course', 3),
+  ('Google', 'http://google.com','An indie search engine startup', 4),
+  ('Fluffiest Cats in the World', 'http://medium.com/bloggerx/fluffiest-cats-334','The only list of fluffy cats online', 5),
+  ('Craigslist', 'https://portland.craigslist.org/','Marketplace',4),
+  ('Flexbox Froggy','https://flexboxfroggy.com/','Css flexbox tricks', 5),
+  ('Stack overflow', 'https://stackoverflow.com/','Great website for developers',5),
+  ('Etymolgy', 'https://www.etymonline.com/','Website for words',3),
+  ('TripAdvisor','https://www.tripadvisor.com/','Trip help', 4),
+  ('All Trails', 'https://www.alltrails.com/','Hiking trails information',4),
+  ('Airbnb','https://www.airbnb.com/', 'Rent cheap houses in different cities',5);
