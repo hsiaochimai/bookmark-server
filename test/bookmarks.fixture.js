3f58f9931129ac4cb456b0a9f9bfdb372150049d
Jennifer Hsiao <jchmai21@gmail.com>
2019-05-07 07:04:35 -0700

    get bookmarks test passed

---
+++ test/bookmarks.fixture.js
@@ -0,0 +1,29 @@
+function makeBookmarksArray(){
+    return [
+        
+            {
+                "id": 1,
+                "title": "Test 1",
+                "url_link": "https://www.test1.com/",
+                "descript": "Test 1",
+                "rating": 3
+            },
+            {
+                "id": 2,
+                "title": "Test 2",
+                "url_link": "http://test2.com",
+                "descript": "Test 2",
+                "rating": 4
+            },
+            {
+                "id": 3,
+                "title": "Test 3",
+                "url_link": "http://test3.com/",
+                "descript": "Test 3",
+                "rating": 5
+            }
+    ]
+}
+module.exports ={
+ makeBookmarksArray,   
+}
